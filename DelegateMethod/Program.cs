5ad805e4375b1654d3de86aa4617293e78185943
PrabhanshuLakshakar07 <lakshakarprabhanshu@gmail.com>
2026-05-23 18:21:11 +0530

    LINQ

---
+++ DelegateMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DelegateMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ LINQ Real/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_Real
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
