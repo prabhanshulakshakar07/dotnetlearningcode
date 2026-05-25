7e2a34994aa42277e1bad50ab5269cb17c955b6e
PrabhanshuLakshakar07 <lakshakarprabhanshu@gmail.com>
2026-05-25 15:36:12 +0530

    Disconnected Arch

---
+++ DisconnectArch_Demo38th/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DisconnectArch_Demo38th
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
