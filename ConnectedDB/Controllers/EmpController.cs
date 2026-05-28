b328b6641adc72828b73ba78f46afa484682563d
PrabhanshuLakshakar07 <lakshakarprabhanshu@gmail.com>
2026-05-28 10:57:44 +0530

    CRUD

--- ConnectedDB/Controllers/EmpController.cs
+++ ConnectedDB/Controllers/EmpController.cs
@@ -12,11 +12,78 @@ namespace ConnectedDB.Controllers
         {
             _DbContext = context;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
             List<Emp> emps = _DbContext.emps.ToList();
             return View(emps);
         }
+
+        [HttpGet]     // /Home/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult InsertRecord(Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _DbContext.emps.Add(emp);
+                _DbContext.SaveChanges();
+
+
+            }
+
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Emp empTobeEdited = _DbContext.emps.Find(id);
+
+            if (empTobeEdited == null)
+            {
+                return NotFound();
+            }
+
+            return View(empTobeEdited);
+        }
+
+
+        [HttpPost]   // /Home/UpdateRecord
+        public IActionResult UpdateRecord(Emp empTobeEdited)
+        {
+            if (ModelState.IsValid)
+            {
+                Emp toBeupdated = _DbContext.emps.Find(empTobeEdited.Id);
+                toBeupdated.Name = empTobeEdited.Name;
+                toBeupdated.Address = empTobeEdited.Address;
+                _DbContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+
+
+
+
+        }
+
+        // /Home/Delete/id
+        public IActionResult Delete(int id)
+        {
+            Emp empTobeDeleted = _DbContext.emps.Find(id);
+            _DbContext.emps.Remove(empTobeDeleted);
+            _DbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
     }
 }
